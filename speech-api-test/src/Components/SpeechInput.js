41b7ad24aae416a55357e55397086ebc6b9167cf
Bryce-Soghigian <brybrycodingguy@gmail.com>
2019-12-25 22:19:15 -0600

    made use of speech input and speech recognition

---
+++ speech-api-test/src/Components/SpeechInput.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import { useSpeechSynthesis } from "react-speech-kit";
+
+export default function SpeechInput() {
+  const [value, setValue] = useState("");
+  const { speak } = useSpeechSynthesis();
+
+  return (
+    <div>
+      <textarea
+        value={value}
+        onChange={event => setValue(event.target.value)}
+      />
+      <button onClick={() => speak({ text: value })}>Speak</button>
+    </div>
+  );
+}
+
+
