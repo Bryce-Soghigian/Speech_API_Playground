307b67e46f16b59cb0b208c22b584db2a62c0392
Bryce-Soghigian <brybrycodingguy@gmail.com>
2019-12-25 22:35:30 -0600

    added production build

---
+++ speech-api-test/build/precache-manifest.3b2303e4579af55aa64a53cbfa574263.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "780801e51525b95d0d9cbc3ac39dced2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27402ba0646a8b8d6250",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8ad8d3ac61bc347c3e96",
+    "url": "/static/js/2.fa4c2585.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.fa4c2585.chunk.js.LICENSE"
+  },
+  {
+    "revision": "27402ba0646a8b8d6250",
+    "url": "/static/js/main.6b1906ba.chunk.js"
+  },
+  {
+    "revision": "8bb7a6909f2ae7ffbaa7",
+    "url": "/static/js/runtime-main.1c86be3c.js"
+  }
+]);
